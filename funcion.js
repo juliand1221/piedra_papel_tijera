014d1ad78417647c5419a867501302440b9c4903
Julian David Tenorio <judt1221@hotmail.com>
2023-06-16 00:54:31 -0500

    Arreglo de css

--- funcion.js
+++ funcion.js
@@ -1,8 +1,7 @@
-let inicioJuego = document.getElementById('escoger')
-let radioBtns = document.querySelectorAll('input[type="radio"]');
-let botonJuego = document.getElementById('boton-juego')
-let nombre = document.getElementById('nombre')
-let juegoPC = aleatorio(1,3);
+let botonRoca = document.getElementById('roca')
+let botonPapel = document.getElementById('papel')
+let botonTijera = document.getElementById('tijera')
+
 
 let inputRoca = document.getElementById('roca')
 let inputPapel = document.getElementById('papel')
@@ -15,53 +14,71 @@ let imgEnemigo = document.getElementById('imgEnemigo')
 
 let jugador 
 let oponente
+let resultado
+let resultadoFinal
 
 let vidasEnemigo = 3
 let vidasJugador = 3
+let spanVidasJugador = document.getElementById('vidasJugador')
+let spanVidasEnemigo = document.getElementById('vidasEnemigo')
 let barravidaJugador = document.getElementById('vidJugador')
 let barraVidaEnemigo = document.getElementById('vidEnemigo')
 
-let resultado = document.getElementById('resultado')
+let cartaUno = document.getElementById('cartaUno')
+let cartaDos = document.getElementById('cartaDos')
 
+let sectionMensaje = document.getElementById('mensajeResultado')
+let sectionMensajeFinal = document.getElementById('mensajeFinal')
+
+let cartas = document.getElementById('escoger')
+
+let botonReinicio = document.getElementById('botonReinicio')
 
 function juego(){
+    botonReinicio.style.display = 'none'
 
-    botonJuego.style.display = 'none'
+    botonRoca.addEventListener('click', ataquepiedra )
+    botonPapel.addEventListener('click', ataquePapel)
+    botonTijera.addEventListener('click', ataqueTijera)
 
-    for (var i=0; i<radioBtns.length;i++){
-        radioBtns[i].addEventListener('change',function(){
-            if(this.checked){
-                botonJuego.style.display='flex';
-            } else{
-                botonJuego.style.display='none';
-            }
-        });
-    }
-
-    botonJuego.addEventListener('click', seleccionarJuego)
+    botonReinicio.addEventListener('click', reiniciarJuego)
 
 }
+
+
+
+function ataquepiedra(){
+    jugador = 'Piedra'
+    seleccionarJuego();
+}
+function ataquePapel(){
+    jugador = 'Papel'
+    seleccionarJuego();
+}
+function ataqueTijera(){
+    jugador = 'Tijera'
+    seleccionarJuego()
+}
+
 function seleccionarJuego(){
 
-    if(inputRoca.checked){
+    if(jugador == 'Piedra'){
         spanJuego.innerHTML = 'Piedra'
         imgJugador.src = '/images/mole.png'
-        jugador = 'Piedra'
         seleccionarPc();
-    }else if(inputPapel.checked){
+    }else if(jugador == 'Papel'){
         spanJuego.innerHTML = 'Papel'
         imgJugador.src = '/images/papel.png'
-        jugador = 'Papel'
         seleccionarPc();
-    }else if(inputTijera.checked){
+    }else if(jugador == 'Tijera'){
         spanJuego.innerHTML = 'Tijera'
         imgJugador.src = '/images/Tijera.png'
-        jugador = 'Tijera'
         seleccionarPc();
     }
 
 }
 function seleccionarPc(){
+    let juegoPC = aleatorio(1,3);
     if(juegoPC == 1){
         spanPc.innerHTML = 'Piedra'
         imgEnemigo.src= '/images/mole.png'
@@ -79,26 +96,75 @@ function seleccionarPc(){
 }
 function enfrentamiento(){
     if(jugador == oponente){
-        alert('Empate')
+        // alert('Empate')
+        resultado = 'Empate'
     }else if(jugador == 'Piedra' && oponente == 'Tijera'){
         vidasEnemigo--
-        barraVidaEnemigo.src = '/images/corazon2.png'
-        alert('ganas')
+        resultado = 'Ganaste'
+        revisarVidasEnemigo();
+        // alert('ganas')
     }else if(jugador == 'Papel' && oponente == 'Piedra'){
         vidasEnemigo--
-        barraVidaEnemigo.src = '/images/corazon2.png'
-        alert('ganas')
+        resultado = 'Ganaste'
+        revisarVidasEnemigo();
+        // alert('ganas')
     }else if(jugador == 'Tijera' && oponente == 'Papel'){
         vidasEnemigo--
-        barraVidaEnemigo.src = '/images/corazon2.png'
-        alert('ganas')
+        resultado = 'Ganaste'
+        revisarVidasEnemigo();
+        // alert('ganas')
     }else{
         vidasJugador --
-        barravidaJugador.src = '/images/corazon2.png'
-        alert('pierdes')
+        resultado = 'Perdiste'
+        revisarVidasJugador();
+        // alert('pierdes')
+    }
+    mensaje();
+
+}
+function revisarVidasEnemigo(){
+
+    if(vidasEnemigo == 2){
+        barraVidaEnemigo.src = '/images/corazon2.png'
+    }
+    else if( vidasEnemigo == 1){
+        barraVidaEnemigo.style.width = '80px'
+        barraVidaEnemigo.src = '/images/corazon1.png'
+    }
+    else if(vidasEnemigo == 0){
+        cartaDos.style.display = 'none'
+        resultadoFinal = 'Haz Ganado'
+        mensajeFinal();
+        // alert('ganaste todoooo')
     }
 }
+function revisarVidasJugador(){
+    if(vidasJugador == 2){
+        barravidaJugador.src = '/images/corazon2.png'
+    }
+    else if(vidasJugador==1){
+        barravidaJugador.style.width = '80px'
+        barravidaJugador.src = '/images/corazon1.png'
+    }else if(vidasJugador == 0){
+        cartaUno.style.display = 'none'
+        resultadoFinal = 'Lastimosamente Perdiste'
+        mensajeFinal();
+        // alert('Perdiste todooo')
+    }
+}
+function mensaje(){
+    sectionMensaje.innerHTML = resultado
+}
+function mensajeFinal(){
+    botonReinicio.style.display = 'flex'
+    sectionMensaje.style.display = 'none'
+    cartas.style.display = 'none'
+    sectionMensajeFinal.innerHTML = resultadoFinal
+}
 
+function reiniciarJuego(){
+    location.reload()
+}
 function aleatorio(min,max){
     return Math.floor(Math.random()*(max - min +1)+ min)
 }
